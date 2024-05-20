9f38ebfc09a449b333299ca4e15e60b37386121f
vinir <46539805+malkyn@users.noreply.github.com>
2024-05-20 20:18:45 -0300

    TextoInformativo

--- IndieVerso-Front/IndieVerso/src/app/pages/home/home.component.ts
+++ IndieVerso-Front/IndieVerso/src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  backgroundColor: string = 'blue'; // Cor de fundo inicial
+  logoSrc: string = '../../../../assets/images/logoindieversovermelho.png'; // Logo inicial
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
+    debugger
+    this.updateLogoBasedOnBackgroundColor();
+    const bannerElement = this.el.nativeElement.querySelector('.banner');
+    const observer = new MutationObserver(() => {
+      this.updateLogoBasedOnBackgroundColor();
+    });
+    observer.observe(bannerElement, { attributes: true, attributeFilter: ['style'] });
   }
 
+
+ updateLogoBasedOnBackgroundColor(): void {
+    const bannerElement = this.el.nativeElement.querySelector('.banner');
+    const backgroundColor = getComputedStyle(bannerElement).backgroundColor;
+
+    const colorLogoMap: { [key: string]: string } = {
+      'rgb(255, 0, 0)': '../../../../assets/images/logoindieversoazul.png', // vermelho
+      'rgb(0, 0, 255)': '../../../../assets/images/logoindieversovermelho.png', // azul
+      // Adicione mais mapeamentos de cores e logos conforme necessário
+    };
+
+    if (colorLogoMap[backgroundColor]) {
+      this.logoSrc = colorLogoMap[backgroundColor];
+    } else {
+      this.logoSrc = 'path/to/default/logo.png'; // Logo padrão
+    }
+}
 }
